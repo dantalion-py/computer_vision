06d262944d6a5f3769d1085231cdeef93dbcc1f9
dantalion-py <userdantalion@gmail.com>
2024-01-27 11:07:53 -0400

    first_commit

---
+++ test.py
@@ -0,0 +1,20 @@
+
+
+import cv2
+
+# Importa la imagen
+img = cv2.imread("img/tatsumakiCHU.png")
+
+
+# Convierte la imagen a RGB
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+
+# Muestra la imagen
+cv2.imshow("Imagen", img)
+
+# Espera a que el usuario presione una tecla
+cv2.waitKey(0)
+
+# Cierra la ventana
+cv2.destroyAllWindows()
